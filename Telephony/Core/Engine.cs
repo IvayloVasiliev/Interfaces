9c4b296396417062a28ad8ea274737ab04da2a6f
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-14 17:43:09 +0200

    Telephony

---
+++ Telephony/Core/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony.Core
+{
+    public class Engine
+    {
+        public void Run()
+        {
+            Smartphone smartphone = new Smartphone();
+
+            string[] inputNumbers = Console.ReadLine().Split();
+            string[] inputUrls = Console.ReadLine().Split();
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                smartphone.Call(inputNumbers[i]);
+            }
+
+            for (int i = 0; i < inputUrls.Length; i++)
+            {
+                smartphone.Browse(inputUrls[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Telephony/Smartphone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telephony.Contracts;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrawsebale
+    {
+        public void Browse(string url)
+        {
+            if (url.Any(x => char.IsDigit(x)))
+            {
+                Console.WriteLine("Invalid URL!");
+                
+            }
+            else 
+            {
+                Console.WriteLine($"Browsing: {url}");
+            }
+        }
+
+        public void Call(string number)
+        {
+            if (number.All( x => char.IsDigit(x)))
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+}
